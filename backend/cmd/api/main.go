0b294df075f3662bc9601ac584ebc253b3c7257b
Alexey Lars <mariawhowells@gmail.com>
2025-10-09 21:11:40 +0300

    feat: add config load
    Реализована структура конфига, его загрузка в main

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/AlexeyLars/surway-service/internal/config"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	// Configure logging
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	slog.SetDefault(logger)
+
+	// Load cfg
+	cfg, err := config.Load()
+	if err != nil {
+		logger.Error("failed to load config", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	logger.Info("Starting service",
+		slog.String("server_address", cfg.Server.Address()),
+	)
+
+}
