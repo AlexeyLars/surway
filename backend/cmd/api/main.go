12d31c998e39796561807676ed35565cf84879fc
Alexey Lars <mariawhowells@gmail.com>
2025-10-09 21:27:11 +0300

    feat: add poll models and redis storage realization
    Создан интерфейс хранилища и его Redis-реализация, а также модели данных, связанные с голосованием

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"github.com/AlexeyLars/surway-service/internal/config"
+	"github.com/redis/go-redis/v9"
 	"log/slog"
 	"os"
+	"time"
 )
 
 func main() {
@@ -22,4 +25,24 @@ func main() {
 		slog.String("server_address", cfg.Server.Address()),
 	)
 
+	// Connect Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     cfg.Redis.Address(),
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.DB,
+	})
+
+	// Redis connection check
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		logger.Error("failed to connect to redis",
+			slog.String("address", cfg.Redis.Address()),
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
+	}
+
+	logger.Info("Connected to Redis", slog.String("redis_address", cfg.Redis.Address()))
 }
---
+++ backend/internal/model/poll.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"time"
+)
+
+type Poll struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Options   []string  `json:"options"`
+	CreatedAt time.Time `json:"created_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+type CreatePollRequest struct {
+	Title   string   `json:"title" binding:"required,min=3,max=200"`
+	Options []string `json:"options" binding:"required,min=2,max=10,dive,required,min=1,max=100"`
+}
+
+type CreatePollResponse struct {
+	PollID     string `json:"poll_id"`
+	VoteURL    string `json:"vote_url"`
+	ResultsURL string `json:"results_url"`
+}
+
+type VoteRequest struct {
+	OptionIndex []int `json:"option_index" binding:"required"`
+}
+
+type VoteResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+}
+
+type PollResults struct {
+	Poll  Poll           `json:"poll"`
+	Votes map[string]int `json:"votes"` // option -> count
+	Total int            `json:"total"`
+}
+
+type ErrorResponse struct {
+	Error   string `json:"error"`
+	Message string `json:"message,omitempty"`
+}
